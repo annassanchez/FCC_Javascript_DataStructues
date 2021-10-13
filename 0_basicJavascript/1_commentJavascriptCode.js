dd1111b690c929bf3bf9eede902d292f677dc043
anna s <annassanchez@gmail.com>
2021-10-13 14:49:03 +0200

    new folder created

---
+++ 0_basicJavascript/1_commentJavascriptCode.js
@@ -0,0 +1,18 @@
+//comments are lines of code that JS will ingore
+//comments are a great way to leave notes to yourself or others that check later your code
+
+//there are two ways to write comments in JavaScript
+
+//Using // will tell Javascript to ignore the remainder of the rext on the current line. This is an inline colum
+// This is an in-line comment.
+
+//You can make a multiline comment beginning with /* an ending with */ -- a multiline comment
+/* This is a
+multi-line comment */
+
+//Note: good commenting can help communicate the intent of your code-- both others and to your future self
+
+//my frist comment
+
+/*my second
+comment*/
