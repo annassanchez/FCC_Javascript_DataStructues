9f053e87b8f33a21f21eeb348e31e31e07cb303e
anna s <annassanchez@gmail.com>
2021-10-14 13:26:00 +0200

    7_caseSensitivityVariables added

---
+++ 0_basicJavascript/7_caseSensitivityVariables.js
@@ -0,0 +1,14 @@
+//it is recommended using camelCase for writing variable names
+var someVariable;
+var anotherVariableName;
+var thisVariableNameIsSoLong;
+//example:
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
