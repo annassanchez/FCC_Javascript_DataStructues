d042a5fb1f4dee0651d4f256eab4fc5f37a8785a
anna s <annassanchez@gmail.com>
2021-10-14 12:41:23 +0200

    3_assginmentOperator added

---
+++ 0_basicJavascript/3_assignmentOperator.js
@@ -0,0 +1,8 @@
+//assignment operator (=)
+myVariable = 5;
+//first the code creates the variable and the in gets assgined
+var Var;
+myVar = 5;
+//assign the value 7 to a variable a
+var a;
+a = 7;
