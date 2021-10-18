bc01e8d6a86911656bffc478465dee4710269c5d
anna s <annassanchez@gmail.com>
2021-10-18 10:54:57 +0200

    12_numberIncrementation added

---
+++ 0_basicJavascript/12_numberIncrementation.js
@@ -0,0 +1,8 @@
+//incrementation can be done with the ++ operator
+i++;
+//it is similar to 
+i = i + 1;
+//example myVar needs to be 88
+var myVar = 87;
+// Only change code below this line
+myVar++;
