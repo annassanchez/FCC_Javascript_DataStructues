9fe465cc9ec78504728de6ab6e0a51a5fbd8fcd3
anna s <annassanchez@gmail.com>
2021-10-18 12:03:16 +0200

    19_compoundAssignmentAugmentedSubtraction added

---
+++ 0_basicJavascript/19_compoundAssignmentAugmentedSubtraction.js
@@ -0,0 +1,12 @@
+//writing
+myVar = myVar - 5;
+//is equivalent to
+myVar -= 5;
+//exercise: a should be 5, b should be -6 and c should be 2
+var a = 11;
+var b = 9;
+var c = 3;
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
