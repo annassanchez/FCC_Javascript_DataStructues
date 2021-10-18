762a1a9e1aa021e0eca71212a94256f48b376b83
anna s <annassanchez@gmail.com>
2021-10-18 11:53:04 +0200

    18_compoundAssignmentAugmentedAddition added

---
+++ 0_basicJavascript/18_compoundAssignmentAugmentedAddition.js
@@ -0,0 +1,14 @@
+//writing
+myVar = myVar + 5;
+//is equivalent to
+var myVar = 1;
+myVar += 5;
+console.log(myVar);
+//exercise: a should be 15, b should be 26 and c should be 19
+var a = 3;
+var b = 17;
+var c = 12;
+// Only change code below this line
+a +=12;
+b += 9;
+c += 7;
