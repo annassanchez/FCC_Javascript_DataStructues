89acfc503fe15c2352a9738a43ce3c3a30e5f571
anna s <annassanchez@gmail.com>
2021-10-18 12:23:29 +0200

    21_compoudAssignmentAugmentedDivision added

---
+++ 0_basicJavascript/21_compoudAssignmentAugmentedDivision.js
@@ -0,0 +1,12 @@
+//writing
+myVar = myVar / 5;
+//is equivalent to
+myVar /= 5;
+//exercise: a should be 4, b should be 27 and c should be 3
+var a = 48;
+var b = 108;
+var c = 33;
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
