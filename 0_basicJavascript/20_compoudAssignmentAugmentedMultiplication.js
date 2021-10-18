66970ec6aca8f959b7f047d448692dd65994032b
anna s <annassanchez@gmail.com>
2021-10-18 12:11:15 +0200

    20_compoudAssignmentAugmentedMultiplication added

---
+++ 0_basicJavascript/20_compoudAssignmentAugmentedMultiplication.js
@@ -0,0 +1,12 @@
+//writing
+myVar = myVar - 5;
+//is equivalent to
+myVar *= 5;
+//exercise: a should be 25, b should be 36 and c should be 46
+var a = 5;
+var b = 12;
+var c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
