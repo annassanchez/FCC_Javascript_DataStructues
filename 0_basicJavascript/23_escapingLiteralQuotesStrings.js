de30eccd74583954005682d1707cec4cbc9a92f9
anna s <annassanchez@gmail.com>
2021-10-18 15:04:45 +0200

    23_escapingLiteralQuotesStrings added

---
+++ 0_basicJavascript/23_escapingLiteralQuotesStrings.js
@@ -0,0 +1,4 @@
+//in case you want  to use " or ' within a string, you can use \
+var sampleStr = "Alan said, \"Peter is hearning JavaScript\".";
+//example: write a myStr variable that would print: I am a "double quoted" string inside "double quotes".
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
