0fa6b1fc72533acc8d2c976d05c830bc1ba68d97
anna s <annassanchez@gmail.com>
2021-10-18 11:04:40 +0200

    13_numberDecrementation added

---
+++ 0_basicJavascript/13_numberDecrementation.js
@@ -0,0 +1,8 @@
+//decrementation can be done with the -- operator
+i--;
+//it is similar to 
+i = i - 1;
+//example myVar needs to be 10
+var myVar = 11;
+// Only change code below this line
+myVar--;
