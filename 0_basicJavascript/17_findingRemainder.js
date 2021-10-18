1bd22469125d5caae1dc0e743fd7a3e9edf103b0
anna s <annassanchez@gmail.com>
2021-10-18 11:41:51 +0200

    17_findngRemainder added

---
+++ 0_basicJavascript/17_findingRemainder.js
@@ -0,0 +1,12 @@
+//the remainder operator is %
+//example:
+/*5 % 2 = 1 because
+Math.floor(5 / 2) = 2 (Quotient)
+2 * 2 = 4
+5 - 4 = 1 (Remainder)*/
+//usage: check if a number is odd or even
+/*17 % 2 = 1 (17 is Odd)
+48 % 2 = 0 (48 is Even)*/
+//exercise: set remainder to 11 divided by 3
+var remainder = 11 % 3;
+var remainder;
