4b37ff518625aecf276d921421c69c434968a988
anna s <annassanchez@gmail.com>
2021-10-19 09:32:46 +0200

    26_concatenatingStringsPlusOperator added

---
+++ 0_basicJavascript/26_concatenatingStringsPlusOperator.js
@@ -0,0 +1,4 @@
+//you can concatenate strings with the + operator
+var ourStr = "I come first. " + "I come second.";
+//example: concatenate the strings "This is the start." and "This is the end."
+var myStr = "This is the start. " + "This is the end."; // Change this line
