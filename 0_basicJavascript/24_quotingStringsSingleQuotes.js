1eba3b0057218298f8165caa923731e9405eccdf
anna s <annassanchez@gmail.com>
2021-10-19 08:49:16 +0200

    24_quotingStringsSingleQuote added

---
+++ 0_basicJavascript/24_quotingStringsSingleQuotes.js
@@ -0,0 +1,11 @@
+//for strings is ok to use either " or '
+doubleQuoteStr = "This is a string"; 
+singleQuoteStr = 'This is also a string';
+//both ' and " can be combined
+conversation = 'Finn exclaims to Jake, "Algebraic!"';
+//also to combine both of the quotation marks and using \ for not having an error.
+goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
+/*badStr = 'Finn responds, "Let's go!"';*/
+//example: remove double quotes
+var myStr = '<a href="http://www.example.com/" target="_blank">Link</a>';
+console.log(myStr)
