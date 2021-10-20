c3a55e0cc3bcb989a0f7284950640596708177ac
anna s <annassanchez@gmail.com>
2021-10-20 09:51:54 +0200

    33_bracketNotationFindNthCharacter added

---
+++ 0_basicJavascript/33_bracketNotationFindNthCharacter.js
@@ -0,0 +1,8 @@
+//with bracket notation, you can also access the nth position in a string
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+//example: get the third letter of a string variable
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line
+var thirdLetterOfLastName = lastName[2]; // Change this line
