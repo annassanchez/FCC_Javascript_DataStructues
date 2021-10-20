c3223b36f6b4d73f03ebf1ef1fa7e7301b368cbf
anna s <annassanchez@gmail.com>
2021-10-20 09:15:36 +0200

    30_findStringLength added

---
+++ 0_basicJavascript/30_findStringLength.js
@@ -0,0 +1,8 @@
+//for calculating the length of the string with .length
+console.log("Alan Peter".length);
+//example
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+// Only change code below this line
+lastNameLength = lastName.length;
