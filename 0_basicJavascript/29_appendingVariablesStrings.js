2544e5383a54b804e9080e905c41ac74e58c086b
anna s <annassanchez@gmail.com>
2021-10-20 09:04:15 +0200

    29_appendingVariablesStrings added

---
+++ 0_basicJavascript/29_appendingVariablesStrings.js
@@ -0,0 +1,8 @@
+//string variables can also be appended with the += operator
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+// Change code below this line
+var someAdjective = "cool!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
