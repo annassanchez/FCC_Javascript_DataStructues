9eb01333ef6e3d79de77d3b996bffa957e9c3949
anna s <annassanchez@gmail.com>
2021-10-20 10:59:32 +0200

    35_bracketNotationFindNthToLastCharacter added

---
+++ 0_basicJavascript/35_bracketNotationFindNthToLastCharacter.js
@@ -0,0 +1,7 @@
+//to get the last nth-position of a string variable, you can combine the length of that variable and subtract the nth number
+var firstName = "Augusta";
+var thirdToLastLetter = firstName[firstName.length - 3];
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
