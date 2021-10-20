8149825671eca7c1527bae9f1614d8faa0efc9fa
anna s <annassanchez@gmail.com>
2021-10-20 13:01:09 +0200

    39_accessArrayDataWithIndexes added

---
+++ 0_basicJavascript/39_accessArrayDataWithIndexes.js
@@ -0,0 +1,7 @@
+//to access the arrays you can also use indexes
+var array = [50,60,70];
+array[0];
+var data = array[1];
+//example: the myData varaible should be the first item of the array
+var myArray = [50,60,70];
+var myData = myArray[0];
