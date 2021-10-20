df0bcb8ac5df16dfd54cdfea1ae9717f8e3502da
anna s <annassanchez@gmail.com>
2021-10-20 08:49:33 +0200

    27_concatenateStringsPlusEqualOperator added

---
+++ 0_basicJavascript/27_concatenateStringsPlusEqualOperator.js
@@ -0,0 +1,7 @@
+//you can also concatenate strings with the += operator
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+//example
+// Only change code below this line
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
