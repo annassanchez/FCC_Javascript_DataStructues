360820e15dd9347c5f109d01e798c5496dfec34b
anna s <annassanchez@gmail.com>
2021-10-20 12:05:42 +0200

    37_storeMultipleValuesInArrays added

---
+++ 0_basicJavascript/37_storeMultipleValuesInArrays.js
@@ -0,0 +1,5 @@
+//arrays can store several pieces of data in one place
+var sandwich = ["peanut butter", "jelly", "bread"]
+//example
+// Only change code below this line
+var myArray = ["bacon", 2, "lettuce", "tomato"];
