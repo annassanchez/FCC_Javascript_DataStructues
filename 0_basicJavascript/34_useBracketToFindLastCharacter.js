62e7c1da589729314e7f79432717a1c375b75e3c
anna s <annassanchez@gmail.com>
2021-10-20 10:49:50 +0200

    34_useBracketToFindLastCharacter added

---
+++ 0_basicJavascript/34_useBracketToFindLastCharacter.js
@@ -0,0 +1,8 @@
+//to get the last position of a string variable, you can combine the length of that variable and subtract 1
+var firstName = "Ada";
+var lastLetter = firstName[firstName.length - 1];
+//example:
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length -1]; // Change this line
