58d6a1b0d8e553d0fd61d9c6529ee56d75c6b3f6
anna s <annassanchez@gmail.com>
2021-10-20 08:54:42 +0200

    28_constructingStringsWithVariables added

---
+++ 0_basicJavascript/28_constructingStringsWithVariables.js
@@ -0,0 +1,7 @@
+//string variables can be concatenated with the + operator
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+//concatenate myName and myStr
+// Only change code below this line
+var myName = "anna";
+var myStr = "My name is " + myName + " and I am well!";
