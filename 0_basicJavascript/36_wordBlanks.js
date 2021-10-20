57878bfa4031ec7ec1cda261d006674eba984f56
anna s <annassanchez@gmail.com>
2021-10-20 11:29:40 +0200

    36_wordBlanks added

---
+++ 0_basicJavascript/36_wordBlanks.js
@@ -0,0 +1,10 @@
+//you can concatenate sentences by cincatenating strings with + operator
+var sentence = "It was really " + "hot" + ", and we " + "laughed" + " ourselves " + "silly" + ".";
+//concatenate the different string values
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+// Only change code below this line
+var wordBlanks = "My " + myNoun + " " + myVerb + " very " + myAdverb + " when he saw a " + myAdjective + " mouse."; // Change this line
+// Only change code above this line
