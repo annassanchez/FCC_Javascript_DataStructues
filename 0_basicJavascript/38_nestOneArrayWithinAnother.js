21b9cf63b95ae0664225244ffa0cac9508be02af
anna s <annassanchez@gmail.com>
2021-10-20 12:22:59 +0200

    38_nestOneArrayWithinAnother added

---
+++ 0_basicJavascript/38_nestOneArrayWithinAnother.js
@@ -0,0 +1,4 @@
+//arrays can also be nested within arrays
+[["Bulls", 23], ["White Sox", 45]]
+// Only change code below this line
+var myArray = [["bacon", "lettuce", "tomato"], ["jamón", "queso"]];
