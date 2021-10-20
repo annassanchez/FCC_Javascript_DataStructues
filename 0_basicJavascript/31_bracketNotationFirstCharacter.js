6708930ee4dff3571a035034e7152c2409fb1035
anna s <annassanchez@gmail.com>
2021-10-20 09:28:31 +0200

    31_bracketNotationFirstCharacter added

---
+++ 0_basicJavascript/31_bracketNotationFirstCharacter.js
@@ -0,0 +1,9 @@
+//bracket notation is a way to get character by their index
+var firstName = "Charles";
+var firstLetter = firstName[0];
+//example
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
