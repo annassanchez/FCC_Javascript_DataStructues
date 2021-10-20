d8e1728646016bcf81fd243a7790e1714a0d031e
anna s <annassanchez@gmail.com>
2021-10-20 13:23:18 +0200

    changes added

--- 0_basicJavascript/40_modifyArrayWithIndices.js
+++ 0_basicJavascript/40_modifyArrayWithIndices.js
@@ -2,3 +2,7 @@
 var ourArray = [50,40,30];
 ourArray[0] = 15;
 //example: modify the data stored at index 0 to a value of 45
+// Setup
+var myArray = [18,64,99];
+myArray[0] = 45;
+// Only change code below this line
