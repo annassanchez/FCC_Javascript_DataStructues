c1f34348a902c46ddc27b304e6cdd79357221a87
anna s <annassanchez@gmail.com>
2021-10-20 13:22:34 +0200

    40_modifyArrayWithIndices added

---
+++ 0_basicJavascript/40_modifyArrayWithIndices.js
@@ -0,0 +1,4 @@
+//unlike strings, arrays are mutable and can be changed with indentation
+var ourArray = [50,40,30];
+ourArray[0] = 15;
+//example: modify the data stored at index 0 to a value of 45
