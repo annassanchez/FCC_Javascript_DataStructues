fd359aa42aa7976e46f59346ff0c646ec539d4d6
anna s <annassanchez@gmail.com>
2021-10-21 11:19:35 +0200

    48_passingValuesFunctions added

---
+++ 0_basicJavascript/48_passingValuesFunctions.js
@@ -0,0 +1,9 @@
+//a function can also include variables
+function testFun(param1, param2) {
+    console.log(param1, param2);
+}
+//example: create a function that accepts two arguments and outputs the sum to the dev console
+function functionWithArgs(param1, param2) {
+    console.log(param1 + param2);
+}
+functionWithArgs(1, 2);
