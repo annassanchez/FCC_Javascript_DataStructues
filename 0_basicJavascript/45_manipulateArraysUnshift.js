74d4cb8a2a954894b5028d2ade4153a6444dd874
anna s <annassanchez@gmail.com>
2021-10-21 10:33:42 +0200

    45_manipulateArraysUnshift added

---
+++ 0_basicJavascript/45_manipulateArraysUnshift.js
@@ -0,0 +1,8 @@
+//with unshift, you can add elements to an array at the start of it
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift();
+ourArray.unshift("Happy");
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35])
