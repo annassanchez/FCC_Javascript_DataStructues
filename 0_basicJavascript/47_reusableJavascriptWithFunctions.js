705fd5a67090af619349d83c2a31ac08b9c408d7
anna s <annassanchez@gmail.com>
2021-10-21 10:51:42 +0200

    47_reusableJavascriptWithFunctions added

---
+++ 0_basicJavascript/47_reusableJavascriptWithFunctions.js
@@ -0,0 +1,9 @@
+//you can also invoke functions in javascript
+function functionName() {
+    console.log("Hello World");
+  }
+//example: write a function and call it
+function reusableFunction() {
+    console.log("Hi World");
+  }
+  reusableFunction()
