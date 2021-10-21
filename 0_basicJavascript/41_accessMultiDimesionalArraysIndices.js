f59dc9c2dd37eed32a9ea74f226efc990cc67182
anna s <annassanchez@gmail.com>
2021-10-21 09:15:39 +0200

    41_accessMultiDimesionalArraysIndices added

---
+++ 0_basicJavascript/41_accessMultiDimesionalArraysIndices.js
@@ -0,0 +1,11 @@
+//to access what it tis called an array of arrays, we use indices
+var arr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [[10,11,12], 13, 14]
+  ];
+  arr[3]; //[[10, 11, 12], 13, 14]
+  arr[3][0]; //[10, 11, 12]
+  arr[3][0][1]; //11
+//example: 
