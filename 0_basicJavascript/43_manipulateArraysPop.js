16e8079382538c61b3c7ae59b8de0c7ec3c4eaf1
anna s <annassanchez@gmail.com>
2021-10-21 09:53:27 +0200

    43_manipulateArraysPop added

---
+++ 0_basicJavascript/43_manipulateArraysPop.js
@@ -0,0 +1,11 @@
+//to add more elements to an existing arrays in js, you can use the .pop() funcion
+//unlike .push(), .pop() deletes the last element from the array
+var threeArr = [1, 4, 6];
+var oneDown = threeArr.pop();
+console.log(oneDown);
+console.log(threeArr);
+//example: assing the last item from myArray to removedFromMyArray and use .pop() to delete the last element from myArray's array
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
