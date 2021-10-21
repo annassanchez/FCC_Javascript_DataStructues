8afd4b6ff910db7ab88e831f7193867242a04a8f
anna s <annassanchez@gmail.com>
2021-10-21 10:02:29 +0200

    44_manipulateArraysShift added

---
+++ 0_basicJavascript/44_manipulateArraysShift.js
@@ -0,0 +1,8 @@
+//unlike pop(), shift removes the first element from an array
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+//example: use shift() to delete the frist element from my array and assign this element to another value
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
