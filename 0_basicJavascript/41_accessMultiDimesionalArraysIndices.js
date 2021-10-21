e50141608a7642034b245079056689c31c53762a
anna s <annassanchez@gmail.com>
2021-10-21 09:27:24 +0200

    41_accessMultiDimesionalArraysIndices cheanges added

--- 0_basicJavascript/41_accessMultiDimesionalArraysIndices.js
+++ 0_basicJavascript/41_accessMultiDimesionalArraysIndices.js
@@ -8,4 +8,6 @@ var arr = [
   arr[3]; //[[10, 11, 12], 13, 14]
   arr[3][0]; //[10, 11, 12]
   arr[3][0][1]; //11
-//example: 
+//example: get the 8 within the given array
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+var myData = myArray[2][1];
