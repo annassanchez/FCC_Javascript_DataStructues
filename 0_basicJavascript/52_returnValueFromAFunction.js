53dd586b24d6aa7b6923ca7f5afab8b7508406cc
anna s <annassanchez@gmail.com>
2021-10-22 09:18:45 +0200

    52_returnValueFromAFunction added

---
+++ 0_basicJavascript/52_returnValueFromAFunction.js
@@ -0,0 +1,13 @@
+//you can pass values into a function with arguments
+//you can use a return to send a value back out of a function
+function plusThree(num) {
+    return num + 3;
+  }
+var answer = plusThree(5);
+console.log(answer);
+//create a function timesFive that accepts an argumnent, multiplies it by 5 and returns the new value
+function timesFive(num) {
+    return num * 5;
+  }
+var answer2 = timesFive(5);
+console.log(answer2);
