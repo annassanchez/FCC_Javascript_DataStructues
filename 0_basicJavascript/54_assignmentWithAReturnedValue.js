0cb087473deb378670754d99266253a66772a492
anna s <annassanchez@gmail.com>
2021-10-22 14:36:42 +0200

    54_assignmentWithAReturnedValue added

---
+++ 0_basicJavascript/54_assignmentWithAReturnedValue.js
@@ -0,0 +1,12 @@
+//you can assign variables with an operation on the other side of the equal
+//var ourSum = sum(5,12);
+//console.log(ourSum);
+//example
+// Setup
+var processed = 0;
+function processArg(num) {
+  return (num + 3) / 5;
+}
+// Only change code below this line
+processed = processArg(7);
+console.log(processed);
