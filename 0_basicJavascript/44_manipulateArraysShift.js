1110f3961ae065440f7fbcf75a3fdb8bf66f62b2
anna s <annassanchez@gmail.com>
2021-10-25 09:37:16 +0200

    44_manipulateArraysShift changes added

--- 0_basicJavascript/44_manipulateArraysShift.js
+++ 0_basicJavascript/44_manipulateArraysShift.js
@@ -6,3 +6,4 @@ var removedFromOurArray = ourArray.shift();
 var myArray = [["John", 23], ["dog", 3]];
 // Only change code below this line
 var removedFromMyArray = myArray.shift();
+console.log(removedFromMyArray);
