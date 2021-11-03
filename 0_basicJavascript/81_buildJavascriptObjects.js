46f597321d9c7afd8134bcc804b63944e0e2fd43
anna s <annassanchez@gmail.com>
2021-11-03 11:17:22 +0100

    81_buildJavascriptObjects added

---
+++ 0_basicJavascript/81_buildJavascriptObjects.js
@@ -0,0 +1,23 @@
+//objects are similar to arrays
+//to access and be modified, you access the data in objects through properties
+const cat = {
+	"name": "Whiskers",
+  "legs": 4,
+  "tails": 1,
+  "enemies": ["Water", "Dogs"]
+};
+//another objects
+const anotherObject = {
+	make: "Ford",
+	5: "five",
+	"model": "focus"
+};
+//generate an object myDog
+const myDog = {
+    // Only change code below this line
+    "name": "Pecas",
+    "legs": 3,
+    "tails": 1,
+    "friends": ["Toby", "Hugo", "Lua"]
+    // Only change code above this line
+  };
