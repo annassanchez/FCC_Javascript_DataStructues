f52a0ff6b665f1fb316f57592fd69b0a7087722e
anna s <annassanchez@gmail.com>
2021-11-03 11:32:25 +0100

    82_accessingObjectPropertiesDotNotation added

---
+++ 0_basicJavascript/82_accessingObjectPropertiesDotNotation.js
@@ -0,0 +1,19 @@
+//to access elements of the object with dot notation
+const myObj = {
+    prop1: "val1",
+    prop2: "val2"
+  };
+  
+const prop1val = myObj.prop1;
+const prop2val = myObj.prop2;
+//example: access information within an object
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
