ee9a08120952a97df579561c84f1823c8670adab
anna s <annassanchez@gmail.com>
2021-11-04 16:03:48 +0100

    90_manipulatingComplexObjects added

---
+++ 0_basicJavascript/90_manipulatingComplexObjects.js
@@ -0,0 +1,39 @@
+//complex data structure example:
+const ourMusic = [ //an array
+    { //an object
+      "artist": "Daft Punk",
+      "title": "Homework",
+      "release_year": 1997,
+      "formats": [ 
+        "CD", 
+        "Cassette", 
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+//example: an array with two objects, 4 properties
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
