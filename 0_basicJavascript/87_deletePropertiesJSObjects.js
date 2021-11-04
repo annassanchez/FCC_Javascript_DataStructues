460c6c40ddc927a905837caa7abc92e49acbbaa5
anna s <annassanchez@gmail.com>
2021-11-04 11:41:02 +0100

    87_deletePropertiesJSObjects added

---
+++ 0_basicJavascript/87_deletePropertiesJSObjects.js
@@ -0,0 +1,23 @@
+//you can also delete properties so:
+delete ourDog.bark;
+//example
+const ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"],
+    "bark": "bow-wow"
+  };
+  
+  delete ourDog.bark;
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+  delete myDog.tails;
