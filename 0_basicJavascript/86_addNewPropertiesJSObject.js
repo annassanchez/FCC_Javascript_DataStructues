d07fed4ce3e8117efcf46a6f56acfb5db6f92969
anna s <annassanchez@gmail.com>
2021-11-04 11:29:51 +0100

    86_addNewPropertiesJSObject added

---
+++ 0_basicJavascript/86_addNewPropertiesJSObject.js
@@ -0,0 +1,23 @@
+//you can add new properties to existing objects the same way you modify them
+//you can add items with dot notation
+ourDog.bark = "bow-wow";
+//or bracket notation
+ourDog["bark"] = "bow-wow";
+//example:
+const ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+  };
+  
+  ourDog.bark = "bow-wow";
+//exercise: add bark property to myDog
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.bark = "woof"
