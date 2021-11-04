9e32ff59875985ca0f507a077f4172b88e199220
anna s <annassanchez@gmail.com>
2021-11-04 16:41:48 +0100

    92_accessingNestedArrays added

---
+++ 0_basicJavascript/92_accessingNestedArrays.js
@@ -0,0 +1,43 @@
+//how to access objects within arrays
+const ourPets = [
+    {
+      animalType: "cat",
+      names: [
+        "Meowzer",
+        "Fluffy",
+        "Kit-Cat"
+      ]
+    },
+    {
+      animalType: "dog",
+      names: [
+        "Spot",
+        "Bowser",
+        "Frankie"
+      ]
+    }
+  ];
+  
+  ourPets[0].names[1];
+  ourPets[1].names[0];
+//access the second element in the tree arrays
+const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  const secondTree = myPlants[1].list[1];
