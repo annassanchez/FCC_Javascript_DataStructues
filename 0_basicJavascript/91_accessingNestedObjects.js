b5c8a54d8546f7d8b24c585df80b1ba18b53e208
anna s <annassanchez@gmail.com>
2021-11-04 16:20:39 +0100

    91_accessingNestedObjects added

---
+++ 0_basicJavascript/91_accessingNestedObjects.js
@@ -0,0 +1,30 @@
+//the sub-rpoperties of objects can be accessed by chaining the dot/bracket notation
+const ourStorage = {
+    "desk": {
+      "drawer": "stapler"
+    },
+    "cabinet": {
+      "top drawer": { 
+        "folder1": "a file",
+        "folder2": "secrets"
+      },
+      "bottom drawer": "soda"
+    }
+  };
+  
+  ourStorage.cabinet["top drawer"].folder2;
+  ourStorage.desk.drawer;
+//access the string maps
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
