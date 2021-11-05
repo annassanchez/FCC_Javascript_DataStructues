f085f6e26c4590e869b29086dd95c2ac28f09166
anna s <annassanchez@gmail.com>
2021-11-05 14:18:21 +0100

    94_iterateJavascriptWhileLoops added

---
+++ 0_basicJavascript/94_iterateJavascriptWhileLoops.js
@@ -0,0 +1,18 @@
+//while loops run while an specific condition is true, and stops when it is no longer true
+const ourArray = [];
+let i = 0;
+
+while (i < 5) {
+  ourArray.push(i);
+  i++;
+}
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let a = 5;
+while (a >= 0) {
+    myArray.push(a);
+    a--;
+  }
+console.log(myArray);
