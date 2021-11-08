f10277de50e5a239ec6b1435fee81c4e5fe7e81d
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-11-08 14:01:57 +0100

    97_countBackwardsForLoop added

---
+++ 0_basicJavascript/97_countBackwardsForLoop.js
@@ -0,0 +1,15 @@
+//you can also loop backwards with a for loop
+//for example, we can loop backwards even numbers
+const ourArray = [];
+
+for (let i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
