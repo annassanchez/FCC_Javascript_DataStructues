658a9e2e6d8bdf9d140893972a5d3a030f4eb6bf
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-11-08 14:09:32 +0100

    98_iterateArrayForLoop created

---
+++ 0_basicJavascript/98_iterateArrayForLoop.js
@@ -0,0 +1,7 @@
+//a common task in js is iterate through the contents from an array
+//one way to do it is with a for loop
+const arr = [10, 9, 8, 7, 6];
+
+for (let i = 0; i < arr.length; i++) {
+   console.log(arr[i]);
+}
