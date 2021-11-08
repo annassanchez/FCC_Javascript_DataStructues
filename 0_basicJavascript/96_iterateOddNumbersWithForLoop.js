dbe375f8b0fe588366e46553c336ee47e654c31d
anna s <annassanchez@gmail.com>
2021-11-08 09:26:22 +0100

    96_iterateOddNumbersWithForLoop added

---
+++ 0_basicJavascript/96_iterateOddNumbersWithForLoop.js
@@ -0,0 +1,15 @@
+//for loops can also isterate mith more than one at a time
+//for example we can iterate through even numbers
+const ourArray = [];
+
+for (let i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+//push odd numbers from 1 through 9 to myArray
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
