5e2c383b0940a2e9361eb077f611844158fc04e4
anna s <annassanchez@gmail.com>
2021-11-08 09:11:34 +0100

    95_iterateJavascriptForLoops added

---
+++ 0_basicJavascript/95_iterateJavascriptForLoops.js
@@ -0,0 +1,18 @@
+//for loops run an specific number of times
+//for (a; b; c) where a is the initialization statement, b is the condition statement, c is the final expression
+//the initialization statement executes once befor the loop starts -- it sets up the loop variable
+//the condition statement is evaluated through every loop iteration
+//the final expression is executed at the end of each loop iteration, prior to the next condition check
+const ourArray = [];
+
+for (let i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+//for loop to push the values 1 through 5
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 6; i++) {
+  myArray.push(i)
+}
