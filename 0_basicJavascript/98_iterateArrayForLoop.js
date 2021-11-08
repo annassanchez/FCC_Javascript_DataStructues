063bfaa50fdc4867a8958c9c4d722ce0ec1091ca
anna s <annassanchez@gmail.com>
2021-11-08 15:19:55 +0100

    98_iterateArrayForLoop added

--- 0_basicJavascript/98_iterateArrayForLoop.js
+++ 0_basicJavascript/98_iterateArrayForLoop.js
@@ -5,3 +5,13 @@ const arr = [10, 9, 8, 7, 6];
 for (let i = 0; i < arr.length; i++) {
    console.log(arr[i]);
 }
+//example
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+for (var i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+  console.log(total)
+}
