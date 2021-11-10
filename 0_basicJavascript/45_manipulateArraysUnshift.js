53779d92f59e038561fffb650933243f3726a218
anna s <annassanchez@gmail.com>
2021-11-10 14:51:52 +0100

    45_manipulateArraysUnshift added

--- 0_basicJavascript/45_manipulateArraysUnshift.js
+++ 0_basicJavascript/45_manipulateArraysUnshift.js
@@ -4,5 +4,5 @@ ourArray.shift();
 ourArray.unshift("Happy");
 // Setup
 var myArray = [["John", 23], ["dog", 3]];
-myArray.shift();
-myArray.unshift(["Paul", 35])
+console.log(myArray.shift());
+console.log(myArray.unshift(["Paul", 35]));
