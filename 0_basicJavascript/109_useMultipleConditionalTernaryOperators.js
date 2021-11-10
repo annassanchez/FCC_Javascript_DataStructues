4d80212e4ec36f13adefdb320114628220e7b39e
anna s <annassanchez@gmail.com>
2021-11-10 13:34:30 +0100

    109_useMultipleConditionalTernaryOperators added

---
+++ 0_basicJavascript/109_useMultipleConditionalTernaryOperators.js
@@ -0,0 +1,24 @@
+//also multiple conditional operators can be chained together for multiple conditions
+//here's an if, else if and else statements
+function findGreaterOrEqual(a, b) {
+    if (a === b) {
+      return "a and b are equal";
+    }
+    else if (a > b) {
+      return "a is greater";
+    }
+    else {
+      return "b is greater";
+    }
+  }
+//here's the same function but with multiple conditional operators
+function findGreaterOrEqual(a, b) {
+    return (a === b) ? "a and b are equal" 
+      : (a > b) ? "a is greater" 
+      : "b is greater";
+  }
+function checkSign(num) {
+    return (num === 0) ? "zero" : (num > 0) ? "positive" : "negative";
+  }
+  
+  checkSign(10);
