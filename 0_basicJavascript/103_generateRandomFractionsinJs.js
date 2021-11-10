fe3b60b550e3ebe74178647793d60b92a7eff19f
anna s <annassanchez@gmail.com>
2021-11-10 10:48:25 +0100

    103_generateRandomFractionsinJs added

---
+++ 0_basicJavascript/103_generateRandomFractionsinJs.js
@@ -0,0 +1,13 @@
+//Math.random() in js is a function that generates a random decimal number between 0 incluse and 1 exclusive
+//this function can create random numbers
+function randomFraction() {
+
+    // Only change code below this line
+  
+    var result = 0;
+    while (result === 0) {
+      result = Math.random();
+    }
+    return result;
+    // Only change code above this line
+  }
