9665e0020b90399e00ac5f49f84f3cbd399144c4
anna s <annassanchez@gmail.com>
2021-11-10 11:14:27 +0100

    104_generateRandomWholeNumbersWithJS added explanation

--- 0_basicJavascript/104_generateRandomWholeNumbersWithJS.js
+++ 0_basicJavascript/104_generateRandomWholeNumbersWithJS.js
@@ -1,3 +1,8 @@
+//you can also generate random whole numbers
+//1. use Math.random()
+//2. multiply that random number by 20
+//3. use Math.floor() to round the number to its nearest whole number, like this:
+//Math.floor(Math.random() * 20);
 function randomWholeNum() {
 
     // Only change code below this line
