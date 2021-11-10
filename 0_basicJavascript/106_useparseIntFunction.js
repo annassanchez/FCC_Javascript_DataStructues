1299dd9476205e7ad574f6b17fd0697d70e1dc1d
anna s <annassanchez@gmail.com>
2021-11-10 11:59:41 +0100

    106_useparseIntFunction added

---
+++ 0_basicJavascript/106_useparseIntFunction.js
@@ -0,0 +1,8 @@
+//parseInt() parses a string and returns an integer
+const a = parseInt("007");
+//use the parseInt in the convertInteger so it converts the input str to an integer
+function convertToInteger(str) {
+    return parseInt(str);
+  }
+  
+  convertToInteger("56");
