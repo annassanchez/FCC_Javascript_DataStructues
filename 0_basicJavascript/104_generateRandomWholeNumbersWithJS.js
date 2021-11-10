8e7c3314d61ee3d0e817caf9efbfcd9e16580061
anna s <annassanchez@gmail.com>
2021-11-10 11:03:42 +0100

    104_generateRandomWholeNumbersWithJS added

---
+++ 0_basicJavascript/104_generateRandomWholeNumbersWithJS.js
@@ -0,0 +1,9 @@
+function randomWholeNum() {
+
+    // Only change code below this line
+    var result = 0;
+      while (result === 0) {
+        result = Math.floor(Math.random() * 10);;
+      }
+      return result;
+  }
