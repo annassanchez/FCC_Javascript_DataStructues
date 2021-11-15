27bf9f2ec440f0fee046196d2fa7442f40e1f4a3
anna s <annassanchez@gmail.com>
2021-11-15 17:30:10 +0100

    03_preventObjectMutation added

---
+++ 1_ES6/03_preventObjectMutation.js
@@ -0,0 +1,27 @@
+//const declaration alone does not really protect your data from mutation
+//to ensure it does not change, you habe Object.freeze
+let obj = {
+    name:"FreeCodeCamp",
+    review:"Awesome"
+  };
+  Object.freeze(obj);
+  obj.review = "bad";
+  obj.newProp = "Test";
+  console.log(obj); 
+//example: freeze MATH_CONSTANTS
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
