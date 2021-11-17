c3e597b6d562bda94a3a0a34eb0d4d40b80f902d
anna s <annassanchez@gmail.com>
2021-11-17 16:49:06 +0100

    changes added

--- 1_ES6/10_useDestructuringAssignmentToAssignVariablesFromObjects.js
+++ 1_ES6/10_useDestructuringAssignmentToAssignVariablesFromObjects.js
@@ -2,7 +2,21 @@
 const user = { name: 'John Doe', age: 34 };
 //ES6 assignment for variables in objects
 const { name: userName, age: userAge } = user;
-
+//example: use the destructuring assignment
+//initial function
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+    
+  const highToday = HIGH_TEMPERATURES.today;
+  const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+  
+  // Only change code above this line
+//solution
 const HIGH_TEMPERATURES = {
     yesterday: 75,
     today: 77,
