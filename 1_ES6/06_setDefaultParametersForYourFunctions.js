9b389fb4b09568595e0631fcefe60fd9b18c9b6f
anna s <annassanchez@gmail.com>
2021-11-17 13:57:23 +0100

    changes added

--- 1_ES6/06_setDefaultParametersForYourFunctions.js
+++ 1_ES6/06_setDefaultParametersForYourFunctions.js
@@ -4,7 +4,7 @@ const greeting = (name = "Anonymous") => "Hello " + name;
 console.log(greeting("John")); // "Hello John"
 console.log(greeting()); // "Hello Anonymous"
 //example: modify the increment by adding a default parameter 1
-
+const increment = (number, value) => number + value;
 // Only change code below this line
 const increment = (number, value = 1) => number + value;
 // Only change code above this line
