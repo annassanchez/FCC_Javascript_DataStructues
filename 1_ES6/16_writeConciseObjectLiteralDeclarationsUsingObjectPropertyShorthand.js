3faec7bfcc87b30aa332efd4e96592bd2782566b
anna s <annassanchez@gmail.com>
2021-11-23 08:34:02 +0100

    16_writeConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand added

---
+++ 1_ES6/16_writeConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand.js
@@ -0,0 +1,15 @@
+//ES6 adds some nice support for easily defining object literals -- object porperty shorthand
+const getMousePosition = (x, y) => ({
+    x: x,
+    y: y
+  });
+//getMousePorperties can also be written with one x
+getMousePosition = (x, y) => ({ x, y });
+//example: use object property shorthand for the given function
+//initial status
+//solution
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {name, age, gender}
+    // Only change code above this line
+  };
