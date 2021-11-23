4825a05385ebee1cc5cf784297934bf4a351713f
anna s <annassanchez@gmail.com>
2021-11-23 08:34:57 +0100

    changes added

--- 1_ES6/16_writeConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand.js
+++ 1_ES6/16_writeConciseObjectLiteralDeclarationsUsingObjectPropertyShorthand.js
@@ -7,6 +7,15 @@ const getMousePosition = (x, y) => ({
 getMousePosition = (x, y) => ({ x, y });
 //example: use object property shorthand for the given function
 //initial status
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name: name,
+      age: age,
+      gender: gender
+    };
+    // Only change code above this line
+  };
 //solution
 const createPerson = (name, age, gender) => {
     // Only change code below this line
