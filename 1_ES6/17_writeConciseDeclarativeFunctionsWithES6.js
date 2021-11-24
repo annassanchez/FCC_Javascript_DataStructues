190ae07d33d1e3fdf702d8fb23810f4b6257f1c9
anna s <annassanchez@gmail.com>
2021-11-24 08:50:17 +0100

    17_writeConciseDeclarativeFunctionsWithES6 added

---
+++ 1_ES6/17_writeConciseDeclarativeFunctionsWithES6.js
@@ -0,0 +1,25 @@
+//in es5, we use the keyword function as follows
+const person = {
+    name: "Taylor",
+    sayHello: function() {
+      return `Hello! My name is ${this.name}.`;
+    }
+  };
+//
+const person = {
+    name: "Taylor",
+    sayHello() {
+      return `Hello! My name is ${this.name}.`;
+    }
+  };
+//example: refactor the function setGear 
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+      this.gear = newGear;
+    }
+  };
+  // Only change code above this line
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
