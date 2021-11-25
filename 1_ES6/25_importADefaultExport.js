46fb2a0da30d9961f3616981cbe9b6ecba2455bf
anna s <annassanchez@gmail.com>
2021-11-25 12:36:04 +0100

    25_importADefaultExport added

---
+++ 1_ES6/25_importADefaultExport.js
@@ -0,0 +1,9 @@
+//to import a default export, you need a different import syntax
+//add is the default export of the file
+import add from "./math_functions.js";
+//the syntax differs in one key place -- the curly braces
+//example: import the default export from the given file and give that import the name subtract
+import subtract from "./math_functions.js"; 
+// Only change code above this line
+
+subtract(7,4);
