408265dbaea3a99e4a12eef628afeb9dc1bc4111
anna s <annassanchez@gmail.com>
2021-11-25 13:11:28 +0100

    26_createJavaScriptPromise added

---
+++ 1_ES6/26_createJavaScriptPromise.js
@@ -0,0 +1,11 @@
+//a promise in JS is what it looks like -- you promise to do something, usually asynchronously
+//ehrn the task is complete, you fulfill the promise or your failed to do so
+//promise is a constructor function , you need new to create one
+//it takes a function as its argument with two parameters, resolve and reject
+const myPromise = new Promise((resolve, reject) => {
+
+});
+//example: create a promise makeServerRequest
+const makeServerRequest = new Promise((resolve, reject) => {
+
+});
