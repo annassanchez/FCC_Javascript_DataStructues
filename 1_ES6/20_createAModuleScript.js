c3fa7a1483db3f209d6f472177647c108417eaf7
anna s <annassanchez@gmail.com>
2021-11-25 09:55:02 +0100

    20_createAModuleScript added

---
+++ 1_ES6/20_createAModuleScript.js
@@ -0,0 +1,10 @@
+//js can be more modular, clena and maintainable -- you can connect different parts of js code within html with the module type
+<script type="module" src="filename.js"></script>
+//example: add a document of type module and give it the source file of index.js
+<html>
+  <body>
+    <!-- Only change code below this line -->
+  <script type="module" src="index.js"></script>
+    <!-- Only change code above this line -->
+  </body>
+</html>
