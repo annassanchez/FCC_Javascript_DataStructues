94e964f8346134e9595e22c25126c6b4dc242c02
anna s <annassanchez@gmail.com>
2021-11-29 10:47:51 +0100

    09_matchLettersOfTheAlphabet added

---
+++ 2_regularExpressions/09_matchLettersOfTheAlphabet.js
@@ -0,0 +1,13 @@
+//character sets can also affect a range of characters
+//to match a range of charactes, you can match them with a hyphen characters
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+let bgRegex = /[a-e]at/;
+catStr.match(bgRegex);
+batStr.match(bgRegex);
+matStr.match(bgRegex);
+//example: match all the letters in quoteSample
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
