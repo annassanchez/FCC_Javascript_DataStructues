109e3538e2d37e6103f5e1c84dba1a46b5d0d1ac
anna s <annassanchez@gmail.com>
2021-11-29 10:48:04 +0100

    changes added

--- 2_regularExpressions/08_matchSingleCharacterWithMultiplePossibilities.js
+++ 2_regularExpressions/08_matchSingleCharacterWithMultiplePossibilities.js
@@ -1,7 +1,14 @@
 //you can search for a literal pattern with some flexibilities with character classes
 //character classes allow yoyu to define a group of characters you wish to match placing them within [ ]
-
-
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+bigStr.match(bgRegex);
+bagStr.match(bgRegex);
+bugStr.match(bgRegex);
+bogStr.match(bgRegex);
 //example: use a acharacter class with vowels in your regex to find all the vowels
 let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
 let vowelRegex = /[aeiou]/gi; // Change this line
