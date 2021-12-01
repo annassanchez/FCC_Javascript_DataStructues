f98a39c4243b6e931d7cd0e4191379e182c418b7
anna s <annassanchez@gmail.com>
2021-12-01 15:40:24 +0100

    18_matchAllLettersAndNumbers added

---
+++ 2_regularExpressions/18_matchAllLettersAndNumbers.js
@@ -0,0 +1,13 @@
+//the closest character class match  all of the characters equivalent to [A-Za-z0-9_]
+let longHand = /[A-Za-z0-9_]+/;
+let shortHand = /\w+/;
+let numbers = "42";
+let varNames = "important_var";
+longHand.test(numbers);
+shortHand.test(numbers);
+longHand.test(varNames);
+shortHand.test(varNames);
+//example: use the shorthand character class to count the number of alphanummeric characters
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
