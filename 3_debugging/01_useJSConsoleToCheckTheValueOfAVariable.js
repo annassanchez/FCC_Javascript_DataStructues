9c8e28b17038d992f5875ff5bd9d553b8a346864
anna s <annassanchez@gmail.com>
2021-12-03 13:55:28 +0100

    new folder daded

---
+++ 3_debugging/01_useJSConsoleToCheckTheValueOfAVariable.js
@@ -0,0 +1,11 @@
+//the console.log() method prints the output of wheats within the parentheses to the console
+console.log('Hello world!');
+//example: use console.log() to check the value of a
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a)
+
+let sumAB = a + b;
+console.log(sumAB);
