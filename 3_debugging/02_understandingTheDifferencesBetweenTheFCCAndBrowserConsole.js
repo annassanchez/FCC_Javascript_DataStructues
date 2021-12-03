969379e6bd8a53c835b696552f6edfa86affb325
anna s <annassanchez@gmail.com>
2021-12-03 14:23:38 +0100

    02_understandingTheDifferencesBetweenTheFCCAndBrowserConsole added

---
+++ 3_debugging/02_understandingTheDifferencesBetweenTheFCCAndBrowserConsole.js
@@ -0,0 +1,6 @@
+//console can also be used be used with warn and clear
+//the fcc console will only output log messages, the normal console will output all of them
+//example: use console.clear after the log to clear the borwser console
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output)
+console.clear()
