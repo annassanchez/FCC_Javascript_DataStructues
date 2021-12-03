aa758c7a62960b6fdbf14db3d0a6f67a144fa9dc
anna s <annassanchez@gmail.com>
2021-12-03 14:39:07 +0100

    03_usetypeofToCheckTheTypeOfAVariable added

---
+++ 3_debugging/03_usetypeofToCheckTheTypeOfAVariable.js
@@ -0,0 +1,14 @@
+//you can use typeof to check the data structure or type of a varible
+//this is useful in debugging when working with multiple data types
+//examples of typeof:
+console.log(typeof ""); // returns string
+console.log(typeof 0); // returns number
+console.log(typeof []); // returns object
+console.log(typeof {}); // returns object
+//example: add two console.log statements to see the data types of seven and three
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
