d9b7ee62d344dca48f2a2acc7de064de8f66b7e2
anna s <annassanchez@gmail.com>
2021-12-03 13:18:58 +0100

    33_removeWhitespaceFromStartAndEnd added

---
+++ 2_regularExpressions/33_removeWhitespaceFromStartAndEnd.js
@@ -0,0 +1,4 @@
+//example: write a egex that matches the whitespace at the begging and end of strings
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ""); // Change this line
