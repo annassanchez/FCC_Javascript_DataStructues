4af2623a980e2d22ef076b4e67cc3263b09d7817
anna s <annassanchez@gmail.com>
2021-12-09 09:09:22 +0100

    04_catchMispelledVariableAndFunctionNames added

---
+++ 3_debugging/04_catchMispelledVariableAndFunctionNames.js
@@ -0,0 +1,5 @@
+//example: fix the two spelling in the code so the calculation works
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = recievables - payable;
+console.log(`Net working capital is: ${netWorkingCapital}`);
