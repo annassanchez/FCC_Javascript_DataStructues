8f057d3e259b5000f89bb3fd3435d14d0e48c9e2
anna s <annassanchez@gmail.com>
2021-12-09 09:56:26 +0100

    07_catchUseOfAssignmentOperatorInsteadOfTheEqualityOperator added

---
+++ 3_debugging/07_catchUseOfAssignmentOperatorInsteadOfTheEqualityOperator.js
@@ -0,0 +1,23 @@
+//branching programs that do different things if certain conditions are met, rely of if statements in js
+//the condition sometimes takes the formof testing whether a result is equal to a value
+//the assignment operator = assigns a value to a variable name in js
+//the == and === operator for equality
+let x = 1;
+let y = 2;
+if (x = y) {
+
+	} else {
+
+	}
+//example: fix the conditionm so the programs runs the right branch and is appropriate reassigned to result
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
