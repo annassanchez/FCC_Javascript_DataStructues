85a6e9b3ec608d373672c657aef340748d821355
anna s <annassanchez@gmail.com>
2021-12-10 10:41:51 +0100

    new folder added

---
+++ 4_basicDataStructures/01_useArrayToStoreCollectionOfData.js
@@ -0,0 +1,32 @@
+//example of the simplest implementation of an array data structure
+//known as one-dimensional array, meaning that it has one level
+//it can contain booleans, strings, numbes
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length);
+//example of a more complex implementation
+//this is known as a multi-dimensional array
+//this arrays are also capable of storing complex objects
+let complexArray = [
+    [
+      {
+        one: 1,
+        two: 2
+      },
+      {
+        three: 3,
+        four: 4
+      }
+    ],
+    [
+      {
+        a: "a",
+        b: "b"
+      },
+      {
+        c: "c",
+        d: "d"
+      }
+    ]
+  ];
+//example: assign to an array at least 5 elements and it needs to contain at least a string, a number and a boolean
+let yourArray = ["uno", 2, true, "cuatro", 5];
