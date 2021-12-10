00eec9c6ef6a8f4ac507bf0d4bbd46197785c754
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-12-10 19:57:37 +0100

    08_copyAnArrayWithSpreadOperator added

--- 4_basicDataStructures/08_copyAnArrayWithSpreadOperator.js
+++ 4_basicDataStructures/08_copyAnArrayWithSpreadOperator.js
@@ -1,3 +1,10 @@
+//slice() allows u to be selective baout what elements of an array to copy, the spread operator allows to copy all of an array's statements
+//the spread syntax simply looks like this: ...
+//you can copy an array so:
+let thisArray = [true, true, undefined, false, null];
+let thatArray = [...thisArray]; //returns [true, true, undefined, false, null]
+//thatArray remains undefined
+//example: edit the function so it can copy an array an specified number of times
 function copyMachine(arr, num) {
     let newArr = [];
     while (num >= 1) {
