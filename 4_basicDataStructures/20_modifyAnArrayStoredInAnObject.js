4051b87bcb7709aed28d1a305b508bf9d7b25320
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-12-12 18:02:11 +0100

    20_modifyAnArrayStoredInAnObject added

---
+++ 4_basicDataStructures/20_modifyAnArrayStoredInAnObject.js
@@ -0,0 +1,29 @@
+//example: the given function should be able to add new freinds to the array within the user object
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+      username: 'kennethCodesAllDay',
+      joinDate: 'March 26, 2016',
+      organization: 'freeCodeCamp',
+      friends: [
+        'Sam',
+        'Kira',
+        'Tomo'
+      ],
+      location: {
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'USA'
+      }
+    }
+  };
+  
+  function addFriend(userObj, friend) {
+    // Only change code below this line
+    userObj.data.friends.push(friend);
+    return userObj.data.friends
+    // Only change code above this line
+  }
+  
+  console.log(addFriend(user, 'Pete'));
