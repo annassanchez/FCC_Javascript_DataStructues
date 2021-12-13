ed412c1570e93fa92c7e6573381029f62f9879fa
anna s <annassanchez@gmail.com>
2021-12-13 16:16:30 +0100

    04_findTheLongestWordInAString added

---
+++ 5_basicAlgorithmScripting/04_findTheLongestWordInAString.js
@@ -0,0 +1,13 @@
+//example: return the length of the longest word in the provided sentence
+function findLongestWordLength(str) {
+    var words = str.split(" ");
+    let maxLength = 0;
+    for (var i = 0; i < words.length; i++) {
+      if(words[i].length > maxLength) {
+        maxLength = words[i].length;
+      }
+    }
+    return maxLength;
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
