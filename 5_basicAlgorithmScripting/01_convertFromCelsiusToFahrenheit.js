45b31ffe7f1bed850872e0cfd3040f3d9100c509
anna s <annassanchez@gmail.com>
2021-12-13 15:47:48 +0100

    new folder created

---
+++ 5_basicAlgorithmScripting/01_convertFromCelsiusToFahrenheit.js
@@ -0,0 +1,7 @@
+//example: create a function that converts from celsius to fahrenheit degrees
+function convertToF(celsius) {
+    let fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit;
+  }
+  
+  convertToF(30);
