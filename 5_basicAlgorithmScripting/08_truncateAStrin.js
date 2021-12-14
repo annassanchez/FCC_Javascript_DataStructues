4c3b2450637b23b1e570f8785ede453dc52d1aa8
anna s <annassanchez@gmail.com>
2021-12-14 09:03:22 +0100

    08_truncateAStrin added

---
+++ 5_basicAlgorithmScripting/08_truncateAStrin.js
@@ -0,0 +1,11 @@
+//example: truncate a string if it is longer than the given maximum string length
+//return the truncated string with a ... ending
+function truncateString(str, num) {
+    if (str.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  }
+  
+  truncateString("A-tisket a-tasket A green and yellow basket", 8);
