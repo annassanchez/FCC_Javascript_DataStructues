d8ca3a15346a9750de3d1a9914ae92b4bbc0b586
anna s <annassanchez@gmail.com>
2021-12-14 08:41:30 +0100

    06_confirmTheEnding added

--- 5_basicAlgorithmScripting/06_confirmTheEnding.js
+++ 5_basicAlgorithmScripting/06_confirmTheEnding.js
@@ -1,4 +1,5 @@
-//example: 
+//example: check if a string (first argument, str) wnds with the given target string (second argument, target)
+//this challende can be solved with the .endsWith() -- for the purpose of this challenge, we would use the substrings
 function confirmEnding(str, target) {
     return str.slice(str.length - target.length) === target;
   }
