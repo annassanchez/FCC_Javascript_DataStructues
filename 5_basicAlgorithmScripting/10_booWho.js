710515deac3d933111118d4a5f7439ee624159a1
anna s <annassanchez@gmail.com>
2021-12-15 09:00:07 +0100

    10_booWho added

---
+++ 5_basicAlgorithmScripting/10_booWho.js
@@ -0,0 +1,7 @@
+//example: check if a value is classifie as a boolean primitive. Return true or false
+//boolean primitives are true and false
+function booWho(bool) {
+    return typeof bool === "boolean";
+  }
+  
+  booWho(null);
