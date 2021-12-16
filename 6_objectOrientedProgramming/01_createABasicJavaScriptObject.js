e5408b62c15383bc9272ae0e423d9e8dee81e006
anna s <annassanchez@gmail.com>
2021-12-16 15:39:04 +0100

    new folder added

---
+++ 6_objectOrientedProgramming/01_createABasicJavaScriptObject.js
@@ -0,0 +1,17 @@
+//think about things people see every day, ike cars, shops and birds. these are all objects: tangible things that you observe and interact with
+//some qualites of objects can be wheels, items to be sold, wings for birds
+//these are qualities (properties), define what makes up an object
+//similar objects can share similar properties, but may differ on the values of those properties
+//for example: a car has for wheels but not all cars have the same number of wheels
+//objects in JS are used to model real-world objects, giving them properties and behaviour
+//here's an example with a duck object
+let duck = {
+	name: "Aflac",
+	numLegs: 2
+};
+//this object has two property/value pairs
+//example: create a dog object with name and numLegs properties, and set them respectively to a str and a num
+let dog = {
+    name: "Pepe",
+    numLegs: 4
+  };
