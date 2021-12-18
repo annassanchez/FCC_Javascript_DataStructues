646395e934e9e18fcdac9b713e25c8838a121468
annassanchez <51114581+annassanchez@users.noreply.github.com>
2021-12-18 19:00:00 +0100

    06_useAConstructorToCreateObjects added

---
+++ 6_objectOrientedProgramming/06_useAConstructorToCreateObjects.js
@@ -0,0 +1,17 @@
+function Bird() {
+    this.name = "Albert";
+    this.color  = "blue";
+    this.numLegs = 2;
+  }
+  
+  let blueBird = new Bird();
+//this inside the constructor always refers to the object being created
+//notice that the new operator is used when calling a contructor
+//this tells js to create a new instance of Bird called blueBird
+//without the new operator, this inside the constructor would not point to the newly created object, giving unexpected results
+//now bluebird has all the properties defined inside the bird constructor
+blueBird.name;
+blueBird.color;
+blueBird.numLegs;
+//just like any other object, its properties can be accessed and modified
+//example: use the given constructor and add a new instance to it, assigning it to a variable hound
