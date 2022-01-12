4a90e7d47f9ef88deb4a9d81605431eb9f9065ab
anna s <annassanchez@gmail.com>
2022-01-12 10:51:36 +0100

    15_dropIt added

---
+++ 8_intermediateAlgorithm/15_dropIt.js
@@ -0,0 +1,8 @@
+//given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it
+//then return the sert of the array once the condition is satisfied, otherwise, arr should be returned as an empty array
+function dropElements(arr, func) {
+    let sliceIndex = arr.findIndex(func);
+  return arr.slice(sliceIndex >= 0 ? sliceIndex : arr.length);
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
